622c09f2361577ae658df69f795b980bcf8d924c
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2025-11-23 10:29:54 +0100

    ShowCase => Program use top level statements

--- tests/NTokenizers.ShowCase/Program.cs
+++ tests/NTokenizers.ShowCase/Program.cs
@@ -3,33 +3,23 @@ using NTokenizers.ShowCase;
 using System.IO.Pipes;
 using System.Text;
 
-class Program
+using var pipe = new AnonymousPipeServerStream(PipeDirection.Out);
+using var reader = new AnonymousPipeClientStream(PipeDirection.In, pipe.ClientSafePipeHandle);
+
+var writerTask = Task.Run(async () =>
 {
-    static async Task Main()
+    var rng = new Random();
+    byte[] bytes = Encoding.UTF8.GetBytes(MarkupExample.GetSampleText());
+    foreach (var b in bytes)
     {
-        var markup = MarkupExample.GetSampleText();
-
-        using var pipe = new AnonymousPipeServerStream(PipeDirection.Out);
-        using var reader = new AnonymousPipeClientStream(PipeDirection.In, pipe.ClientSafePipeHandle);
-
-        var writerTask = EmitSlowlyAsync(markup, pipe);
-
-        MarkupTokenizer.Parse(reader, MarkupWriter.Write);
-
-        await writerTask;
-        Console.WriteLine("\nDone.");
+        await pipe.WriteAsync(new[] { b }.AsMemory(0, 1));
+        await pipe.FlushAsync();
+        await Task.Delay(rng.Next(1, 5));
     }
+});
 
-    static async Task EmitSlowlyAsync(string text, Stream output)
-    {
-        var rng = new Random();
-        byte[] bytes = Encoding.UTF8.GetBytes(text);
-        foreach (var b in bytes)
-        {
-            await output.WriteAsync(new[] { b }.AsMemory(0, 1));
-            await output.FlushAsync();
-            await Task.Delay(rng.Next(1, 5));
-        }
-        output.Close();
-    }
-}
+MarkupTokenizer.Parse(reader, MarkupWriter.Write);
+
+await writerTask;
+
+Console.WriteLine("\nDone.");
