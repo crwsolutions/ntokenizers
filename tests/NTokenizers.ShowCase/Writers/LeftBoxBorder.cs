06559f1720f1f416a3918b5e4b4bb9457d1deecc
Colin Wilmans <colin.wilmans@crwsolutions.nl>
2025-11-26 14:38:26 +0100

    Moved LeftBoxBorder to its own file

---
+++ tests/NTokenizers.ShowCase/Writers/LeftBoxBorder.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+using Spectre.Console.Rendering;
+
+internal sealed class LeftBoxBorder : BoxBorder
+{
+    public override string GetPart(BoxBorderPart part) => part switch
+    {
+        BoxBorderPart.TopLeft => "│ ",
+        BoxBorderPart.Left => "│ ",
+        BoxBorderPart.BottomLeft => "│ ",
+        _ => " "
+    };
+}
